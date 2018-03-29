77d82db4bee2006ebabbaa82e1243f27a3f5dda1
Anthony Fisher <apf312@nyu.edu>
2018-03-29 17:08:06 -0400

    .

---
+++ test_code/PWM_test/PWM_test.ino
@@ -0,0 +1,19 @@
+#define PWM_PIN 23;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set pin 23 to output, don't think this is actually necessary
+  pinMode(PWM_PIN, OUTPUT);
+  analogWriteFrequency(PWM_PIN, 50);
+  analogWriteResolution(10);
+  analogWrite(PWM_PIN, 512);
+}
+
+void loop() {
+  // There must be a register storing the current output value. Which is it?
+  // GPIOC_PDOR bit 2? Something in FTMx_CnV? Bits 15:0 maybe? I think that is the duty cycle.
+  // FTMx_SWOCTRL bits 8-15 corresponding to channel n software output control value
+  Serial.println((GPIOC_PDOR >> 2) & 1U);
+
+}
---
+++ test_code/README.md
@@ -0,0 +1,1 @@
+Code used for testing, experimentation, examples, etc.
