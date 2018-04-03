fc00af0dc5c108718ba7c7b08577cdb79ef33680
Anthony Fisher <apf312@nyu.edu>
2018-04-02 22:33:02 -0400

    Tested the buzzer and mic with the code in test_code/mic_test/. It seems to correctly pick up different frequencies emitted by the buzzer.

--- test_code/PWM_test/PWM_test.ino
+++ test_code/PWM_test/PWM_test.ino
@@ -1,39 +1,49 @@
+// Use 50Hz frequency for wheel control
 // With 10-bit resolution, write values to use for our wheels are:
 //    Fastest ccw: 51
-//    Stopped: 76
+//    Stopped: 71-81
 //    Fastest cw: 102
 
-#define PWM_PIN 23
+// With 16-bit resolution:
+//    Fastest ccw: 3276
+//    Stopped: ~4540-5210
+//    Fastest cw: 6554
+//
+//    cw is forwards for right wheel, backwards for left wheel
+
+#define PWM_PIN 3
 
 void setup() {
   Serial.begin(115200);
 
   // Set pin 23 to output, don't think this is actually necessary
   pinMode(PWM_PIN, OUTPUT);
-  analogWriteFrequency(PWM_PIN, 50);  // (pin, frequency in Hz)
+  analogWriteFrequency(PWM_PIN, 15000);  // (pin, frequency in Hz)
   analogWriteResolution(10);          // (# of bits)
-  analogWrite(PWM_PIN, 102);          // (pin, 0 .. 2^(# of bits))
+  analogWrite(PWM_PIN, 512);          // (pin, 0 .. 2^(# of bits))
 }
 
 void loop() {
-  // There must be a register storing the current output value. Which is it?
-  // GPIOC_PDOR bit 2? Nope.
-  // Duty cycle is in FTM0_C1V 
-  //   FTM0_C1V[15:0] = 59941 when 1023/1024 duty cycle
-  //                    30000 when 50% duty cycle (512/1024)
-  //                    29941 when 511/1024
-  //                    15000 when 25% duty cycle (256/1024)
-  //                    0 when 100% duty cycle  
-  // Default MOD (maximum timer value) is 60000
-  // FTMx_SWOCTRL bits 8-15 corresponding to channel n software output control value? Nope.
-  
-//  Serial.print("MOD = ");
-//  Serial.println(FTM0_MOD & 0xFFFF);
-//  Serial.print("Value = ");
-//  Serial.println(FTM0_C1V & 0xFFFF);
-//  Serial.print("Initial value = ");
-//  Serial.println(FTM0_CNTIN & 0xFFFF);
-//  Serial.println(FTM0_SWOCTRL, 16);
 
+  Serial.print("FTM1_C0V[15:0] = ");
+  Serial.println(FTM1_C0V & 0xFFFF);
+  Serial.print("FTM1_MOD[15:0] = ");
+  Serial.println(FTM1_MOD & 0xFFFF);
+  Serial.print("FTM1_CNT[15:0] = ");
+  Serial.println(FTM1_CNT & 0xFFFF);
+  Serial.print("FTM1_OUTMASK[0] = ");
+  Serial.println(FTM1_OUTMASK & 1);
+  Serial.print("FTM1_CH0 = ");
+  Serial.println();
   delay(2000);
+
+  /* These two for loops go through the full range at 16-bit resolution */
+//  for (int i = 3276; i <= 6554; ++i) {
+//    analogWrite(PWM_PIN, i);
+//    delay(15);
+//  }
+//  for (int i = 6553; i >= 3276; --i) {
+//    analogWrite(PWM_PIN, i);
+//    delay(15);
+//  }
 }
---
+++ test_code/mic_test/mic_test.ino
@@ -0,0 +1,59 @@
+#define PWM_PIN 3
+
+#include <Audio.h>
+
+AudioInputAnalog adc1;
+AudioAnalyzeToneDetect f1;
+AudioAnalyzeToneDetect f2;
+AudioAnalyzePeak peak;
+
+AudioConnection patchCord1(adc1, f1);
+AudioConnection patchCord2(adc1, peak);
+AudioConnection patchCord3(adc1, f2);
+
+void setup() {
+  Serial.begin(115200);
+  AudioMemory(12);
+
+  // Set pin 23 to output, don't think this is actually necessary
+  pinMode(PWM_PIN, OUTPUT);
+  analogWriteFrequency(PWM_PIN, 6000);  // (pin, frequency in Hz)
+  analogWriteResolution(10);          // (# of bits)
+  analogWrite(PWM_PIN, 512);          // (pin, 0 .. 2^(# of bits))
+
+  f1.frequency(6000);
+  f2.frequency(8000);
+
+}
+
+void loop() {
+  Serial.println();
+
+  if (f1.available()) {
+    Serial.print("f1Val = ");
+    Serial.println(f1.read());
+  }
+
+  if (f2.available()) {
+    Serial.print("f2Val = ");
+    Serial.println(f2.read());
+  }
+
+  if (peak.available()) {
+    Serial.print("Peak value: ");
+    Serial.println(peak.read());
+  }
+
+
+  delay(2000);
+
+  /* These two for loops go through the full range at 16-bit resolution */
+//  for (int i = 3276; i <= 6554; ++i) {
+//    analogWrite(PWM_PIN, i);
+//    delay(15);
+//  }
+//  for (int i = 6553; i >= 3276; --i) {
+//    analogWrite(PWM_PIN, i);
+//    delay(15);
+//  }
+}
